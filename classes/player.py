fb9610a2b70c6d8177361276e96539a62948d2ee
Skyfiv3 <baptrgtt@gmail.com>
2024-01-29 14:17:12 +0100

    Création de my_turn()

--- classes/player.py
+++ classes/player.py
@@ -2,23 +2,28 @@ from multiprocessing import Array ,Value, Semaphore
 from os import system, kill, getpid
 import signal
 import signal
+import socket
 
 class Player :
-    def __init__(self,id,hands,lock,game_lock,suites,draw_pile,discard_pile,number_players,fuse,info):
+    class Player:
+        def __init__(self, id, hands, lock, game_lock, suites, draw_pile, discard_pile, number_players, fuse, info):
+            self.id = id
+            self.number_players = number_players
+            self.lock = lock
+            self.game_lock = game_lock
 
-        self.id = id
-        self.number_players = number_players
-        self.lock = lock
-        self.game_lock=game_lock
+            self.suites = suites
+            self.draw_pile = draw_pile
+            self.discard_pile = discard_pile
 
-        self.suites = suites
-        self.draw_pile = draw_pile
-        self.discard_pile = discard_pile
-        
-        self.fuse = fuse
-        self.info = info
-        
-        self.colors = ["red","blue","green","black","white"]
+            self.fuse = fuse
+            self.info = info
+
+            self.colors = ["red", "blue", "green", "black", "white"]
+
+            # Initialize TCP session
+            self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.tcp_socket.connect(("localhost", 1234))
         
 
 
@@ -31,18 +36,17 @@ class Player :
             kill(pid, signal.SIGKILL)
 
     def draw_card(self,index):
-        #envoi un message au game pour piocher  une carte et la mettre dans le tas de main du joueur a l'index donné
-        
-        pass        
+        message = f"Le joueur {self.id} veut piocher une carte"
+        self.tcp_socket.send(message.encode())
 
 
 
     def give_info(self,neighbor):
-        chek_choice=False
-        while not chek_choice:
+        check_choice=False
+        while not check_choice:
             choice = input("Voulez-vous donner une information sur une couleur ou un chiffre ? ").lower()
             if choice =="couleur" or choice=="chiffre":
-                chek_choice=True
+                check_choice=True
             else:
                 print ("Choix incorrect")
         
@@ -54,6 +58,8 @@ class Player :
                     valid_color = True
                 else:
                     print("Couleur invalide. Veuillez choisir une couleur parmi :", self.colors)
+            message = f"Envoie un indice sur la couleur {color_input} au joueur {neighbor}"
+            self.tcp_socket.send(message.encode())
             
             # Envoyer l'info a game ou au joueur
             
@@ -67,17 +73,20 @@ class Player :
                     print("Chiffre invalide. Veuillez choisir un chiffre entre 1 et 5.")
             
             # Envoyer l'info a game ou au joueur
+            message = f"Envoie un indice sur le chiffre {number_input} au joueur {neighbor}"
+            self.tcp_socket.send(message.encode())
             
         else:
             print("Choix non valide. Veuillez choisir 'couleur' ou 'chiffre'.")
 
 
     def discard(self,index):
-        if self.hand[index]!=-1:
+        if index<len(self.hand):
             card=self.hand[index]
             self.discard_pile.append(card)
             self.hand[index]=-1
-            print(f"La carte :{card} a ete defaussee")
+            message = f"Le joueur {self.id} defausse la carte {card}"
+            self.tcp_socket.send(message.encode())
         else :
             print(f"La carte n'existe pas")
         self.draw_card(index)
@@ -88,10 +97,10 @@ class Player :
                 card_color=card//5
                 card_number=card%5+1
 
-                vide=True
+                vide=False
                 #On test si la liste souhaitée est vide 
                 if len(self.suites[index_suites])==0:
-                    vide=False
+                    vide=True
 
                 test_color=True
                 for i in range(len(list(self.suites))):
@@ -109,15 +118,19 @@ class Player :
                         if card_number==1:
                             self.suites[index_suites].append(card)
                             self.hand.pop(index_card)
+                            message = f"Le joueur {self.id} joue la carte {card} dans la pile {index_suites}"
+                            self.tcp_socket.send(message.encode())
 
                         else:
                             self.discard(index_card)
                     else: 
-                        last_card=self.suites[index_suites][len(self.suites[index_suites])]
+                        last_card=self.suites[index_suites][len(self.suites[index_suites])-1]
                         #Si notre carte est bien la carte de la meme couleur incrementée de 1 on la place, sinon on la jette
                         if last_card//5==card_color and last_card%5+1==card_number-1:
                             self.suites[index_suites].append(card)
                             self.hand.pop(index_card)
+                            message = f"Le joueur {self.id} joue la carte {card} dans la pile {index_suites}"
+                            self.tcp_socket.send(message.encode())
                         else:
                             self.discard(index_card)
 
